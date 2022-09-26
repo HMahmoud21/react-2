dcd6eec42e5d7815ce0c76ae423de7edd8b2c03b
HMahmoud21 <hajermahmoud52@gmail.com>
2022-09-26 22:01:36 +0200

    profile

---
+++ src/Component/profile/ProfilePhoto.js
@@ -0,0 +1,10 @@
+import React from "react";
+import profile from "../../src/img/hajer.jpg"
+function profile () {
+ return (
+   <>
+     <img src ={profile} alt =" photo"/>
+   </>
+ );
+};
+export default profile;
---
+++ src/Component/profile/add.js
@@ -0,0 +1,13 @@
+import React from "react";
+const Address="abab"
+function add (){
+ return (
+   <>
+    <h1>
+        my add {Address}
+    </h1>
+
+   </>
+ );
+};
+export default add ;
